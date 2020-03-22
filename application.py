bf03cdf8842da00992868c339202941703d5072f
Mafc88 <mafc88@gmail.com>
2020-03-22 17:58:21 -0600

    cambios en el login registro y perfil, verificacion de consultas

--- application.py
+++ application.py
@@ -1,16 +1,12 @@
-import os
-from flask import Flask, render_template, redirect, url_for, session, logging, request
+from flask import Flask, render_template, url_for, session, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
 app = Flask(__name__)
 app.secret_key = "shit"
 
 motor = create_engine(
-    "postgres://tybnzuug:sEd-YLwtFP6juILFYUkAKmwTtHpoNasU@drona.db.elephantsql.com:5432/tybnzuug")
+    'postgres://tybnzuug:sEd-YLwtFP6juILFYUkAKmwTtHpoNasU@drona.db.elephantsql.com:5432/tybnzuug')
 db = scoped_session(sessionmaker(bind=motor))
-
-# listar vuelos
 
 
 @app.route("/")
@@ -21,62 +17,91 @@ def index():
 @app.route('/login', methods=["GET", "POST"])
 def login():
     mensaje = ''
+    sesion = ''
     if request.method == "POST" and 'usuario' in request.form and 'password' in request.form:
         usuario = request.form['usuario']
         password = request.form['password']
-        login = db.execute("SELECT * FROM usuarios WHERE usuario = :usuario AND password = :password",
-                           {"usuario": usuario, "password": password}).fetchone()
+        cuenta = db.execute('SELECT * FROM usuarios WHERE usuario= :usuario AND password= :password',
+                            {"usuario": usuario, "password": password}).fetchone()
 
-        # If account exists in accounts table in out database
-        if login:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['usuario'] = login['usuario']
-            # Redirect to home page
-            return 'Logged in successfully!'
+        # si la cuenta existe en la base de datos y concuerda la pass
+        if cuenta:
+            # crea sesion con el nombre de usuario
+            session['logeado'] = True
+            session['usuario'] = cuenta['usuario']
+            session['password'] = cuenta['password']
+            # si todo salio bien redirecciona a pagina de perfil
+            return redirect(url_for('perfil'))
         else:
-            # Account doesnt exist or username/password incorrect
-            mensaje = 'Incorrect username/password!'
+            # si la los datos no coninciden..
+            mensaje = '¡Verifique sus datos!'
 
-    return render_template('index.html', mensaje='')
+    return render_template('index.html', mensaje=mensaje, sesion=sesion)
 
 
-@app.route('/pythonlogin/register', methods=['GET', 'POST'])
-def register():
-    # Output message if something goes wrong...
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    # variable mensaje para mandar mensajes...
     mensaje = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    # verifica si el usuario y password del form existen o no en la bd
     if request.method == 'POST' and 'usuario' in request.form and 'password' in request.form:
-        # Create variables for easy access
+        # crea variables de usuario y password para facilitar la consulta
         usuario = request.form['usuario']
         password = request.form['password']
-        db.execute('SELECT * FROM accounts WHERE username = %s', (username))
-        cuenta = db.fetchone()
-        # If account exists show error and validation checks
-        if cuenta:
-            msg = 'Account already exists!'
-        elif not re.match(r'[A-Za-z0-9]+', usuario):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password:
-            msg = 'Please fill out the form!'
+        account = db.execute("SELECT * FROM usuarios WHERE usuario= :usuario",
+                             {"usuario": usuario}).fetchone()
+        # verifica si la cuenta existe, se agrega el mensaje acorde
+        if account:
+            mensaje = '¡Ya existe una cuenta con ese usuario!'
+        elif not usuario or not password:
+            mensaje = '¡Por favor rellene los campos!'
         else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            db.execute('INSERT INTO accounts VALUES (%s, %s)', (usuario, password))
+            # si la cuenta no existe y los datos son validos se ingresaran los datos en la bd
+            db.execute("INSERT INTO usuarios (usuario,password) VALUES (:usuario, :password)",
+                       {"usuario": usuario, "password": password})
             db.commit()
-            msg = 'You have successfully registered!'
+            mensaje = 'Se registro con exito!'
+            return redirect(url_for('login'))
     elif request.method == 'POST':
-        # Form is empty... (no POST data)
-        mensaje = 'Please fill out the form!'
-    # Show registration form with message (if any)
+        # si el formulario esta vacio muestre mensaje
+        mensaje = '¡Por favor rellene los campos!'
+
     return render_template('registro.html', mensaje=mensaje)
+
+
+@app.route('/home')
+def home():
+    # comprueba si esta activo el login
+    if 'logeado' in session:
+        cuenta = session['usuario']
+
+        # si el usuario esta logeado abrir pagina principal o home
+        return render_template('home.html', cuenta=cuenta)
+
+    # si no lo esta redireccionar al login
+    return redirect(url_for('login'))
+
+
+@app.route('/perfil')
+def perfil():
+    # verifica si esta activo el login
+    if 'logeado' in session:
+        # query para obtener todos los datos del usuario
+        # datos = db.execute('SELECT * FROM usuarios WHERE usuario= :usuario',
+        # {"usuario": session['usuario']}).fetchone()
+        cuenta = session['usuario']
+        # muestra la informacion del usuario en el perfil.
+        return render_template('perfil.html', cuenta=cuenta)
+
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
-    # Remove session data, this will log the user out
-    session.pop('loggedin', None)
-    session.pop('id', None)
+    # limpia los datos del session
+    session.pop('logeado', None)
     session.pop('ususario', None)
 
-    # Redirect to login page
+    # redirecciona al login
     return redirect(url_for('login'))
